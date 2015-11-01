d233c2d1c65a4cb4d489891535b1b8cac18bb862
mjc92 <devnote5676@naver.com>
2015-11-01 13:54:23 +0900

    Update generator.java
    특수문자 반영 및 미반영 버전 제작
    길이를 입력하면 해당길이만큼 제작(8이하를 입력하면 길이가 8로 설정됨)
    주어진 string에서 길이만큼 random하게 뽑아냄
    main 함수형태로 실행 확인

--- generator.java
+++ generator.java
@@ -4,20 +4,44 @@
  
 import java.util.*;
 
-class Generator{
-    public static void main(String arg[]){
-        String str=new String("abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ23456789");
+public class Generator {
+	/*조건
+	 * 1: 비밀번호 길이를 미리 입력해야 한다
+	 * 2: 특수문자 포함 여부를 반영할 수 있어야 한다
+	 * 3: 연속되는 세 개 이상의 숫자/문자가 없어야 한다
+	 */
+	public static void main(String arg[]){
+		Scanner scan = new Scanner(System.in);
+		String str=new String("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+		String strsp=new String("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*(),.?/[]{};:|-=_+");
+		
         StringBuffer sb=new StringBuffer();
         String ar=null;
         Random r = new Random();
-        int te=0;
-        for(int i=1;i<=8;i++){
-            te=r.nextInt(62);
-            ar=ar+str.charAt(te);
-            sb.append(str.charAt(te));
+        int length;
+        int enablesp=-1;
+        System.out.println("Set password length: ");
+        length = scan.nextInt();
+        while(enablesp!=0 && enablesp!=1){
+        System.out.println("Set special characters(1-enable, 0-disable): ");
+        enablesp = scan.nextInt();
         }
-        String abc="";
-        char arr[]=str.toCharArray();
+               if(length<8)
+        	length=8;
+        sb=new StringBuffer(length);
+        int te=0;
+        for(int i=0;i<length;i++){
+        	if(enablesp==1){
+        		te=r.nextInt(strsp.length());
+//				ar=ar+strsp.charAt(te);
+        		sb.append(strsp.charAt(te));
+        	}
+        	else{
+        		te=r.nextInt(str.length());
+//				ar=ar+str.charAt(te);
+        		sb.append(str.charAt(te));
+        	}
+        }
         System.out.println(sb.toString());
-    }
-}  
+    }	
+}
