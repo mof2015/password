2129144d07845da3bf41ccac0ff1a98eaa89a13f
ZestIsBest <hyugi316@naver.com>
2015-11-24 22:43:37 +0900

    Search function implemented!

--- gui/SearchForm.java
+++ gui/SearchForm.java
@@ -1,4 +1,5 @@
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JSlider;
@@ -8,63 +9,35 @@ import javax.swing.event.ChangeListener;
 
 public class SearchForm extends JFrame {
 
-	JLabel la_name, la_id, la_pw, la_link;
-	 JTextField tf_name, tf_id, tf_pw, tf_link;
+	JLabel la_box;
+	 JTextField tf;
 	 JButton bt_input, bt_cancel;
+	 JComboBox box = new JComboBox(new Object[] {"Name", "ID", "Password", "link"});
 	 JTextField textField;
 	 ChangeListener listener;
 	 
 	 public SearchForm() {
-	     /**
-		 listener = new ChangeListener()
-	     {
-			public void stateChanged(ChangeEvent e) {
-		           JSlider source = (JSlider) e.getSource();
-		           textField.setText("" + source.getValue());
-			}
-	     };
-	     */
+
 		  setTitle("Insert");
-		  la_name = new JLabel("Name");
-		  la_id = new JLabel("id");
-		  la_pw = new JLabel("pw");
-		  la_link = new JLabel ("link");
 		  
-		  tf_name = new JTextField();
-		  tf_id = new JTextField();
-		  tf_pw = new JTextField();
-		  tf_link = new JTextField();
+		  la_box = new JLabel ("Choose to Search");
+		  
+		  tf = new JTextField();
 		  
 		  bt_input = new JButton("OK");
 		  bt_cancel = new JButton("Cancel");
 		 
 		  setLayout(null);
-		  la_name.setBounds(30, 30, 60, 30);
-		  la_id.setBounds(30, 80, 30, 30);
-		  la_pw.setBounds(30, 130, 30, 30);
-		  la_link.setBounds(30,230,30,30);
-		  
-		  tf_name.setBounds(80, 30, 120, 30);
-		  tf_id.setBounds(80, 80, 120, 30);
-		  tf_pw.setBounds(80, 130, 120, 30);
-		  tf_link.setBounds(80, 230, 200, 30);
-		  
+
+		  la_box.setBounds(30, 30, 120, 30);
+		  box.setBounds(30, 80, 150, 30);
+		  tf.setBounds(30, 120 , 250, 30);
 		  bt_input.setBounds(30, 300, 60, 30);
 		  bt_cancel.setBounds(100, 300, 120, 30);
 		  
-		  //textField = new JTextField();
-		  //textField.setBounds(310,170,50,30);
-		  //add(textField);
-		
-		  add(la_name);
-		  add(la_id);
-		  add(la_pw);
-		  add(la_link);
-
-		  add(tf_name);
-		  add(tf_id);
-		  add(tf_pw);
-		  add(tf_link);
+		  add(la_box);
+		  add(box);
+		  add(tf);
 		  add(bt_input);  
 		  add(bt_cancel);
 		        
@@ -75,12 +48,14 @@ public class SearchForm extends JFrame {
 	 
 	 public void initSearch(){//Search form
 		 setTitle("Search");
+		 /**
 		  tf_name.setText("");
 		  tf_id.setText("");
 		  tf_pw.setText("");
 		  tf_link.setText("");
 		  tf_name.requestFocus();
+		  */
+		 tf.setText("");
+		 tf.requestFocus();
 		 }
 }
-
-
