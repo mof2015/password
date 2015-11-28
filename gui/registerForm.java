9bd32c5ec6b219702570909d4b7f429496beff67
ZestIsBest <hyugi316@naver.com>
2015-11-28 21:55:00 +0900

    OK and Cancel action

--- gui/registerForm.java
+++ gui/registerForm.java
@@ -1,4 +1,3 @@
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -6,6 +5,7 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.event.ChangeEvent;
@@ -14,9 +14,9 @@ import javax.swing.event.ChangeListener;
 
 public class registerForm extends JFrame{
  
-	 JLabel la_mail, la_pw, la_confirm, la_favo;
-	 JTextField tf_mail;
-	 JPasswordField tf_pw, tf_confirm;
+	 JLabel la_id, la_pw, la_confirm, la_favo;
+	 JTextField tf_id;
+	 JTextField tf_pw, tf_confirm;
 	 JButton bt_input, bt_cancel;
 	
 	 public registerForm() {
@@ -24,26 +24,26 @@ public class registerForm extends JFrame{
 			JComboBox favoList = new JComboBox(favorites);
 		
 	  setTitle("Register");
-	  la_mail = new JLabel("e-Mail address");
+	  la_id = new JLabel("ID");
 	  la_pw = new JLabel("Master password");
 	  la_confirm = new JLabel ("Confirm");
 	  la_favo = new JLabel ("Favorite");
 	  
-	  tf_mail = new JTextField();
-	  tf_pw = new JPasswordField();
-	  tf_confirm = new JPasswordField();
+	  tf_id = new JTextField();
+	  tf_pw = new JTextField();
+	  tf_confirm = new JTextField();
 	  
 	  bt_input = new JButton("OK");
 	  bt_cancel = new JButton("Cancel");
 	 
 	  setLayout(null);
 	  
-	  la_mail.setBounds(30, 30, 120, 30);
+	  la_id.setBounds(30, 30, 120, 30);
 	  la_pw.setBounds(30, 70, 120, 30);
 	  la_confirm.setBounds(30,110,120,30);
 	  la_favo.setBounds(30,150,120,30);
 	  
-	  tf_mail.setBounds(160, 30, 120, 30);
+	  tf_id.setBounds(160, 30, 120, 30);
 	  tf_pw.setBounds(160, 70, 120, 30);
 	  tf_confirm.setBounds(160,110,120,30);
 	  
@@ -51,12 +51,12 @@ public class registerForm extends JFrame{
 	  bt_cancel.setBounds(150, 200, 100, 30);
 	  favoList.setBounds(160,150,120,30);
 	  
-	  add(la_mail);
+	  add(la_id);
 	  add(la_pw);
 	  add(la_confirm);
 	  add(la_favo);
 	
-	  add(tf_mail);
+	  add(tf_id);
 	  add(tf_pw);
 	  add(tf_confirm);
 	  add(favoList);
@@ -64,6 +64,32 @@ public class registerForm extends JFrame{
 	  add(bt_input);  
 	  add(bt_cancel);
 	        
+	  bt_input.addActionListener(new ActionListener (){
+		  public void actionPerformed(ActionEvent e) {
+			  String id = tf_id.getText();
+			  String pw = tf_pw.getText();
+			  String confirm = tf_confirm.getText();
+			  
+			  if(id.length() == 0)
+			  {
+				  JOptionPane.showMessageDialog(null, "Input name!!"); 
+				  return;
+			  }
+			  else if(pw.length() == 0)
+			  {
+				  JOptionPane.showMessageDialog(null, "Input password!!"); 
+				  return;
+			  }
+			  else if(confirm.length() == 0)
+			  {
+				  JOptionPane.showMessageDialog(null, "Confirm your password!!"); 
+				  return;
+			  }
+			  
+				setVisible(false);
+		  }
+	  });
+	  
 	  bt_cancel.addActionListener(new ActionListener (){
 		  public void actionPerformed(ActionEvent e) {
 				setVisible(false);
@@ -80,4 +106,3 @@ public class registerForm extends JFrame{
 	
 		
  } 
-
