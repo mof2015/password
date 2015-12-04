d812c41daf86db97bf5e9f2eb4dba3d2167281c7
godix <godpum@gmail.com>
2015-12-04 16:18:24 +0900

    Update EditEmailForm.java

--- db_connect/EditEmailForm.java
+++ db_connect/EditEmailForm.java
@@ -13,37 +13,34 @@ public class EditEmailForm extends JFrame {
 	JTextField tf_email;
 	JButton bt_input, bt_cancel;
 	ChangeListener listener;
-	 
+
 	public EditEmailForm() {
 
+		// Set UI components
 		setTitle("Edit Master Email");
-		  
-		la_box = new JLabel ("Edit Email");
-		  
+		la_box = new JLabel("Edit Email");
 		tf_email = new JTextField();
-		  
 		bt_input = new JButton("OK");
 		bt_cancel = new JButton("Cancel");
-		 
 		setLayout(null);
-		
-		//Component setting and addition
+
+		// Component setting and addition
 		la_box.setBounds(30, 30, 120, 30);
-		tf_email.setBounds(30, 80 , 250, 30);
+		tf_email.setBounds(30, 80, 250, 30);
 		bt_input.setBounds(30, 300, 60, 30);
 		bt_cancel.setBounds(100, 300, 120, 30);
-	  
+
 		add(la_box);
 		add(tf_email);
-		add(bt_input);  
+		add(bt_input);
 		add(bt_cancel);
-		        
+
 		setBounds(500, 500, 400, 400);
-		setResizable(false);    
-	 }
-	 
-	//Edit master email form
-	public void initEditEmail(){
+		setResizable(false);
+	}
+
+	// Edit master email form
+	public void initEditEmail() {
 		setTitle("Edit Master email");
 
 		tf_email.setText("");
